eca3ca8c6e96581226eef53fd36452fe38405475
Mason <mknichol@udel.edu>
2026-02-21 09:08:21 -0500

    completed isQuestion function

--- src/functions.ts
+++ src/functions.ts
@@ -38,7 +38,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let ln = message.length;
+    return message[ln - 1] === "?";
 }
 
 /**
