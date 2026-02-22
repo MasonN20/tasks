7400227f92590f794345ef95e07f0bab8b6d9e2a
Mason <mknichol@udel.edu>
2026-02-22 10:02:23 -0500

    Removed import Last

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,5 +1,3 @@
-import { Last } from "react-bootstrap/esm/PageItem";
-
 /**
  * Consume an array of numbers, and return a new array containing
  * JUST the first and last number. If there are no elements, return
