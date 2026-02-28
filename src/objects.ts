15bd0ca5a4896212a4cde4a98c64d8ff6535617f
Mason <mknichol@udel.edu>
2026-02-28 11:36:16 -0500

    Completed isValid, toShortForm functions

--- src/objects.ts
+++ src/objects.ts
@@ -42,7 +42,13 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "multiple_choice_question") {
+        return question.options.some(
+            (option: string): boolean => option === answer,
+        );
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -52,7 +58,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return `${question.id}: ${question.name.slice(0, 10)}`;
 }
 
 /**
