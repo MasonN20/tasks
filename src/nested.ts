32e45995e8ab2cb8b5edee2a9739653cfff92602
Mason <mknichol@udel.edu>
2026-03-01 18:08:09 -0500

    completed renameQuestionById, changeQuestionTypeById functions

--- src/nested.ts
+++ src/nested.ts
@@ -151,7 +151,9 @@ export function renameQuestionById(
     targetId: number,
     newName: string,
 ): Question[] {
-    return [];
+    return questions.map((question) =>
+        question.id === targetId ? { ...question, name: newName } : question,
+    );
 }
 
 /***
@@ -166,7 +168,18 @@ export function changeQuestionTypeById(
     targetId: number,
     newQuestionType: QuestionType,
 ): Question[] {
-    return [];
+    return questions.map((question) =>
+        question.id === targetId ?
+            {
+                ...question,
+                type: newQuestionType,
+                options:
+                    newQuestionType === "multiple_choice_question" ?
+                        question.options
+                    :   [],
+            }
+        :   question,
+    );
 }
 
 /**
