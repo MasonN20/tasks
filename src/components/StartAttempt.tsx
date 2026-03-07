8156699f2ddf29abf880ee297f9f69e6ada821bd
Mason <mknichol@udel.edu>
2026-03-07 10:22:33 -0500

    Completed StartAttempt  file

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,36 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numA, changeA] = useState<number>(4);
+    const [quizP, setP] = useState<boolean>(false);
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setP(true);
+                    changeA(numA - 1);
+                }}
+                disabled={quizP || numA === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    setP(false);
+                }}
+                disabled={!quizP}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    changeA(numA + 1);
+                }}
+                disabled={quizP}
+            >
+                Mulligan
+            </Button>
+            <div>{numA}</div>
+        </div>
+    );
 }
